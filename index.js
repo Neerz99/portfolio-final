23304636c7ba1d842293938d7ea0a45c99ac96d6
iloenzo99@gmail.com <iloenzo99@gmail.com>
2023-11-16 18:30:25 +0100

    bug fixing

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// hides the menu when a link is clicked
 document.querySelectorAll('.menuItems li a').forEach(item => {
     item.addEventListener('click', event => {
         document.querySelector('.checkbox').checked = false;
     });
 });
+
+// // making the opened menu non-scrollable
+// // NOT WORKING YET !!! (CLICKED-ON LINKS MAKES THE WHOLE PAGE NON-SCROLLABLE)
+// document.querySelector('.checkbox').addEventListener('click', event => {
+//     if (document.querySelector('.checkbox').checked) {
+//         document.querySelector('body').style.overflow = 'hidden';
+//     } else {
+//         document.querySelector('body').style.overflow = 'auto';
+//     }
+// });
