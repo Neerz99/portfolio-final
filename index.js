204893749afe858ab546cd7001bd09e48ef40624
iloenzo99@gmail.com <iloenzo99@gmail.com>
2023-11-20 19:35:52 +0100

    added responsive

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ document.querySelectorAll('.menuItems li a').forEach(item => {
     });
 });
 
+function showMore() {
+    let skills = ['skillSql', 'skillPython', 'skillLinux', 'skillApi'];
+    skills.forEach(skill => {
+        let element = document.querySelector(`.${skill}`);
+        if (element.style.display !== 'block') {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    });
+}
+
+
+
 // // making the opened menu non-scrollable
 // // NOT WORKING YET !!! (CLICKED-ON LINKS MAKES THE WHOLE PAGE NON-SCROLLABLE)
 // document.querySelector('.checkbox').addEventListener('click', event => {
